6bbfb85dc7db6dd5489473c8e6f24ccda8015c91
Deepak kumar <kumardeepak46263@gmail.com>
2026-08-15 14:29:22 +0530

    3 question lavel easy is done

---
+++ 100DaysChallenge/Day5/Day5.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+bool ispowerof2(int n){
+    if(n<1){
+        return false;
+    }else if(n==1){
+        return true;
+    }else{
+        while(n%2==0){
+            n = n/2;
+        }
+        
+        if(n==1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
+
+int main(){
+    cout << "this is power of two or not -> " << ispowerof2(15);
+}
